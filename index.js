883b720d90f9d3c0f8f469ca9a0ad4a17de5cf64
Hossen <mdmitho199911@gmail.com>
2022-05-13 23:34:47 +0600

    API naming convention and simple Booking API

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const cors =require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express()
+const port = process.env.PROT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nwhtf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+  const serviceCollection = client.db("doctors-portal").collection("services");
+  const bookingCollection = client.db("doctors-portal").collection("bookings");
+
+app.get('/service',async(req,res)=>{
+  const query ={}
+  const cursor = serviceCollection.find(query)
+  const services = await cursor.toArray()
+  res.send(services);
+})
+
+/****
+ * API Naming Convention
+ * app.get('/booking') //get all booking in this collection .or get more than one or by filter
+ * app.get('/booking/:id')//get a specific booking
+ * app.post('/booking') // add a new booking
+ * app.patch('/booking/:id)//
+ * app.delete('/booking/:id)//
+ * 
+ * 
+ */
+
+
+
+app.post('/booking',async(req,res)=>{
+  const booking = req.body;
+  const query = {treatment:booking.treatment,date:booking.date,patient:booking.patient}
+  const result = await bookingCollection.insertOne(booking)
+  res.send(result)
+})
+
+
+
+
+
+
+
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+  res.send('Hello my website')
+})
+
+app.listen(port, () => {
+  console.log(`my app listening on port ${port}`);
+});
